7723dbea75fe90cc020c0076cce3ac444da6c68e
jeeva <jeevapersonal1101@gmail.com>
2026-05-20 06:58:30 +0530

    Delete HR/HR directory

--- HR/HR/server/routes/proctoringRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { logWarning } from '../controllers/proctoringController.js';
-import { protect } from '../middleware/auth.js';
-
-const router = express.Router();
-
-router.post('/log', protect, logWarning);
-
-export default router;
